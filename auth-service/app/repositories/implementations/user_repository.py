c9a91ad1b7efd0c6bd001d9b4ba731aa7ac32fbb
Akshit Thakkar <akshit@whitelotuscorporation.com>
2026-07-30 14:54:28 +0530

    Impelemented new alembice version and according to that updated all other files implementation

--- auth-service/app/repositories/implementations/user_repository.py
+++ auth-service/app/repositories/implementations/user_repository.py
@@ -21,3 +21,13 @@ class UserRepository(
 
     async def exists_by_email(self, email: str):
         return await self.get_by_email(email) is not None
+    async def get_by_username(self,username:str):
+        result=await self.db.execute(select(User).where(User.username == username))
+        return result.scalar_one_or_none()
+    async def exists_by_username(self,username:str):
+        return await self.get_by_username(username) is not None
+    async def get_by_doc_id(self,doc_id:str):
+        result=await self.db.execute(select(User).where(User.doc_id == doc_id))
+        return result.scalar_one_or_none()
+    async def exists_by_doc_id(self,doc_id:str):
+        return await self.get_by_doc_id(doc_id) is not None
--- auth-service/app/schemas/user.py
+++ auth-service/app/schemas/user.py
@@ -2,20 +2,18 @@ from pydantic import EmailStr, Field
 
 from .common import BaseResponse
 from shared.schemas.base import BaseSchema
+from app.models.user import Role,DocType
 
 
 class UserCreate(BaseSchema):
-    full_name: str = Field(
-        min_length=2,
-        max_length=100,
-    )
-
+    first_name: str = Field(min_length=2,max_length=100,)
+    last_name: str = Field(min_length=2,max_length=100,)
+    username: str = Field(min_length=2,max_length=100,)
+    role:Role=Field(default=Role.USER)
     email: EmailStr
-
-    password: str = Field(
-        min_length=8,
-        max_length=128,
-    )
+    password: str = Field(min_length=8,max_length=128,)
+    doc_id:str=Field(min_length=2,max_length=100)
+    doc_type:DocType
 
 
 class UserLogin(BaseSchema):
@@ -24,7 +22,13 @@ class UserLogin(BaseSchema):
 
 
 class UserResponse(BaseResponse):
-    full_name: str
+    first_name: str
+    last_name: str
+    username:str
     email: EmailStr
     is_active: bool
-    is_superuser: bool
+    role:Role
+    doc_id:str | None = None
+    doc_type:DocType | None=None
+
+    
