e56e4158aef4982134f12f29a91b3492ab1b30b5
Akshit Thakkar <akshit@whitelotuscorporation.com>
2026-08-13 16:45:33 +0530

    -allow origins set to settings corsoriginlist

--- gateway-service/app/main.py
+++ gateway-service/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
+from shared.config.settings import settings
 from shared.exceptions.handlers import register_exception_handlers
 
 from app.api.v1.api import api_router
@@ -14,7 +15,7 @@ app=FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=settings.cors_origins_list,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
